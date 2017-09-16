e7ee216a16bd614cc81b0cedf58424ed2a165d60
ryonakao <rodriguez.nak@gmail.com>
2017-09-16 16:08:10 +0900

    Create a minimum calendar.

---
+++ shiftEducation1.1/CustomCell.swift
@@ -0,0 +1,14 @@
+//
+//  CustomCell.swift
+//  shiftEducation1.1
+//
+//  Created by 中尾涼 on 2017/09/16.
+//  Copyright © 2017年 中尾涼. All rights reserved.
+//
+
+import UIKit
+import JTAppleCalendar
+
+class CustomCell: JTAppleCell {
+    @IBOutlet var dateLabel: UILabel!
+}
