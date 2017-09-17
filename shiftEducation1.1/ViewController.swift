fc6b0287f6b9ffdb35c985ae3f9edbadaa1d848d
ryonakao <rodriguez.nak@gmail.com>
2017-09-17 11:41:14 +0900

    Change color

--- shiftEducation1.1/ViewController.swift
+++ shiftEducation1.1/ViewController.swift
@@ -11,6 +11,12 @@ import JTAppleCalendar
 
 class ViewController: UIViewController {
     @IBOutlet weak var calendarView: JTAppleCalendarView!
+    
+    let outsideMonthColor = UIColor(colorWithHexValue: 0x584a66)
+    let monthColor = UIColor.white
+    let selectedMonthColor = UIColor(colorWithHexValue: 0x3a194b)
+    let currentDateSelectedViewColor = UIColor(colorWithHexValue: 0x4e3f5d)
+    
     let formatter = DateFormatter()
     
 
@@ -22,6 +28,16 @@ class ViewController: UIViewController {
     func setupCalendarView(){
         calendarView.minimumLineSpacing = 0
         calendarView.minimumInteritemSpacing = 0
+    }
+    
+    func handleCellTextColor(view: JTAppleCell?, cellState: CellState){
+        guard let validCell = view as? CustomCell else { return }
+        if validCell.isSelected{
+            validCell.selectedView.isHidden = false
+        }else{
+            validCell.selectedView.isHidden = true
+        }
+
     }
 
     override func didReceiveMemoryWarning() {
@@ -47,15 +63,36 @@ extension ViewController: JTAppleCalendarViewDataSource{
 }
 
 extension ViewController: JTAppleCalendarViewDelegate{
-    // Desplay the cell
+    // Display the cell
     func calendar(_ calendar: JTAppleCalendarView, cellForItemAt date: Date, cellState: CellState, indexPath: IndexPath) -> JTAppleCell {
         let cell = calendar.dequeueReusableJTAppleCell(withReuseIdentifier: "CustomCell", for: indexPath) as! CustomCell
+        
         cell.dateLabel.text = cellState.text
+        
+        handleCellTextColor(view: cell, cellState: cellState)
         return cell
     }
-    // When touch a cell
+    
+    // cellを選択状態にする
     func calendar(_ calendar: JTAppleCalendarView, didSelectDate date: Date, cell: JTAppleCell?, cellState: CellState) {
-        guard let validCell = cell as? CustomCell else { return }
-        validCell.selectedView.isHidden = false
+        handleCellTextColor(view: cell, cellState: cellState)
+    }
+    
+    //cellの選択状態を外す
+    func calendar(_ calendar: JTAppleCalendarView, didDeselectDate date: Date, cell: JTAppleCell?, cellState: CellState) {
+        handleCellTextColor(view: cell, cellState: cellState)
+
+    }
+    
+}
+
+extension UIColor{
+    convenience init(colorWithHexValue value: Int, alpha:CGFloat = 1.0){
+        self.init(
+            red: CGFloat((value & 0xFF0000) >> 16) / 255.0,
+            green: CGFloat((value & 0x00FF00) >> 8) / 255.0,
+            blue: CGFloat(value & 0x0000FF) / 255.0,
+            alpha: alpha
+        )
     }
 }
